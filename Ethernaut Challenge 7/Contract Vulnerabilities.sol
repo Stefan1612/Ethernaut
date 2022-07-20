fdcf768680c1edbebf561066c034b720ecfef446
Stefan Lehmann <85541624+Stefan1612@users.noreply.github.com>
2022-07-20 06:44:13 +0200

    Create Contract Vulnerabilities.sol

---
+++ Ethernaut Challenge 7/Contract Vulnerabilities.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+2
+pragma solidity ^0.6.0;
+
+// Task: Force send ether into the contract
+// Solution: 
+// Every contract can receive ether forcefully with the "seldestruct" function
+// e.g. =>  
+
+
+
+contract Force 
+{/*
+                   MEOW ?
+         /\_/\       
+    ____/ o o \
+  /~____  =ø= /
+ (______)__m_m)
+*/}
