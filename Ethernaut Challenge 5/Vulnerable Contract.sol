50ec4a1428b1bc335ed0ed3fd3854ed59b30e085
Stefan Lehmann <85541624+Stefan1612@users.noreply.github.com>
2022-07-20 06:14:02 +0200

    Update Vulnerable Contract.sol

--- Ethernaut Challenge 5/Vulnerable Contract.sol
+++ Ethernaut Challenge 5/Vulnerable Contract.sol
@@ -10,9 +10,15 @@ contract Token {
     balances[msg.sender] = totalSupply = _initialSupply;
   }
   
-   // Solutions/Vulnerability //
+  
   
   function transfer(address _to, uint _value) public returns (bool) {
+     // Solutions/Vulnerability //
+    // the require statement is redundant because it will always be true. Uint can't go into the negatives, which means if you were to 
+    // calculate 20 - 21 it would UNDERFLOW and equal a very large number ((2**256) - 1).
+    // this error doesn't occur in newer versions of solidity anymore due to the addition of safe math into standard solidity (you could disable this safe function
+    // for gas optimization but it needs to be done carefully).
+  
     require(balances[msg.sender] - _value >= 0);
     balances[msg.sender] -= _value;
     balances[_to] += _value;
