b98aaceb334a9509858741c25c3c313516839298
Stefan Lehmann <85541624+Stefan1612@users.noreply.github.com>
2022-07-29 23:29:40 +0200

    Create sol.js

---
+++ Ethernaut Challenge 17-Recovery/sol.js
@@ -0,0 +1,8 @@
+/*I chessed myself through this challenge by grabbing the address of etherscan with the deyploment transaction hash.
+But that is not the way, the original poster of the challenge intented us to complete it.
+
+I'll will now go through the steps of the normal way to solve this challenge.
+
+
+
+*/
