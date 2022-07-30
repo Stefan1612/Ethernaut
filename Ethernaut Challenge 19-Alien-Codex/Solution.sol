98ba908787039461affbeaef9c93fb566aad0350
Stefan Lehmann <85541624+Stefan1612@users.noreply.github.com>
2022-07-30 22:48:55 +0200

    Update Solution.sol

--- Ethernaut Challenge 19-Alien-Codex/Solution.sol
+++ Ethernaut Challenge 19-Alien-Codex/Solution.sol
@@ -16,6 +16,19 @@ contract.retract();
 
 3. Find out where the owner variable is inside the array.
 
+  
+
+
 4. change owner to our address
+
+  Explanation: We are abusing a flaw in the ABI specs, the array is of type 32bytes (which means it expects values that equal 32bytes). Just inputting an address
+  (which equals 20bytes) wouldn't cut it. To be able to enter an address into an 32bytes Array we abuse the ABI specs which doesn't validate that the length
+  of the array matches the length of the payload (e.g. 0'd out). That means we can add a bunch of 0's (24 in this example to be exact), infront of our address
+  and turn it into an 32bytes large slot and fullfill the array requirements.
+  
+  
+            (declare hex)   (24, 0's)      (remove 0x from our address)
+  content = 0x            + 0.repeat(24) + player.slice(2)
+  
 
 */
