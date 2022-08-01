54feb0e3157cfaf583416e26945073f499a3c811
Stefan Lehmann <85541624+Stefan1612@users.noreply.github.com>
2022-08-01 18:00:08 +0200

    Create Solution.sol

---
+++ Ethernaut Challenge 23-Dex2/Solution.sol
@@ -0,0 +1,13 @@
+/*
+
+1 First Option:
+
+You could "hack" this contract the same way as the contract before (Check dex1)
+
+2. Second Option:
+
+Option we use for some variety, another, malicious contract.
+
+
+
+*/
