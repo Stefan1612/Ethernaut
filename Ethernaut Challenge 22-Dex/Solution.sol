d7d321dfb308579fea65ccd7eb6afd7810ebfcf0
Stefan Lehmann <85541624+Stefan1612@users.noreply.github.com>
2022-08-01 17:39:53 +0200

    Create Solution.sol

---
+++ Ethernaut Challenge 22-Dex/Solution.sol
@@ -0,0 +1,15 @@
+/*
+Solidity inability to comprehend floating points (e.g. 24.44444, rounds down to 24) will be abused here.
+
+In the "getSwaapPrice" function the price is calculated by dividing 2 numbers, which would work fine IF solidity was able to work with decimals.
+
+Fix: Usually tokenprices are * by their decimals (usually 18).
+
+
+Solution: 
+
+Keep on calling swap() with all your available tokens -> due to the rounding you will keep
+ending up with more tokens than you should. After a certain amount of swaps you will end having all tokens of one kind.
+
+
+*/
